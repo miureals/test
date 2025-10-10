03fdfa04577452f2af3303626c583adf1fc046d0
miureals <miu485926@gmail.com>
2025-10-10 09:51:10 +0700

    Update bf.lua

--- bf.lua
+++ bf.lua
@@ -2,6 +2,7 @@ local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local JumpEnabled = false
 local JumpValue = 50
+local SpeedValue = 16
 
 local Window = Rayfield:CreateWindow({
     Name = "MiuHub",
