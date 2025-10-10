0cb0c9548c16fbbf121ee25c8030fde6c6e9707a
miureals <miu485926@gmail.com>
2025-10-11 05:54:26 +0700

    Update bf.lua

--- bf.lua
+++ bf.lua
@@ -14,7 +14,7 @@ local JumpValue = 50
 -- window
 local Window = Rayfield:CreateWindow({
     Name = "MiuHub",
-    Icon = 0,
+    Icon = 111272883332516,
     LoadingTitle = "Wellcome To A Miuhub",
     LoadingSubtitle = "This Script only for a learn",
     LoadingSubtitle = "By Miu",
