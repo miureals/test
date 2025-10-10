e0c88f40ed32b2eb27810ea9023bf60c13b40b7a
miureals <miu485926@gmail.com>
2025-10-10 13:46:42 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,9 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/miureals/test/refs/heads/main/bf.lua"))()
+local PlaceId = game.PlaceId
+local Script
+
+if PlaceId = 2753915549, 4442272183, 7449423635 then
+  Script = "https://raw.githubusercontent.com/miureals/test/refs/heads/main/bf.lua"
+else
+  Script = "https://raw.githubusercontent.com/miureals/test/refs/heads/main/Main.lua"
+end
