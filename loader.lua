0e23cbbea6feeb0eca13812853c6ea7b1932e40c
miureals <miu485926@gmail.com>
2025-10-10 16:23:37 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,11 +1,11 @@
 local PlaceId = game.PlaceId
-local game = {
-  [2753915549} = "https://raw.githubusercontent.com/miureals/test/refs/heads/main/bf.lua",
-  [4442272183} = "https://raw.githubusercontent.com/miureals/test/refs/heads/main/bf.lua",
-  [7449423635} = "https://raw.githubusercontent.com/miureals/test/refs/heads/main/bf.lua",
+local BloxFruit = {
+  [2753915549] = "https://raw.githubusercontent.com/miureals/test/refs/heads/main/bf.lua",
+  [4442272183] = "https://raw.githubusercontent.com/miureals/test/refs/heads/main/bf.lua",
+  [7449423635] = "https://raw.githubusercontent.com/miureals/test/refs/heads/main/bf.lua",
 }
 local defaultScript = "https://raw.githubusercontent.com/miureals/test/refs/heads/main/Main.lua"
 
-local url = Game[PlaceId} or defaultScript
+local url = Game[PlaceId] or defaultScript
 
 loadstring(game:HttpGet(url))()
