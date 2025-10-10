7e068e6fdc52e31c62be8b32e76cf549cf64339f
miureals <miu485926@gmail.com>
2025-10-10 13:30:19 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/miureals/test/refs/heads/main/Main.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/miureals/test/refs/heads/main/bf.lua"))()
