7ab724639efc3b6405933f75b7fe91338dd06c23
miureals <miu485926@gmail.com>
2025-10-11 22:19:27 +0700

    test

--- Grow_A_Garden.lua
+++ Grow_A_Garden.lua
@@ -40,3 +40,13 @@ local Window = Rayfield:CreateWindow({
 local EspTab = Window:CreateTab("Tab Example") -- Title, Image
 local EspSection = EspTap:CreateSection("Main")
 
+local ESP = loadstring(game:HttpGet("https://raw.githubusercontent.com/miureals/test/refs/heads/main/function/gag_utils.lua"))()
+
+local esp = EspTab:CreateToggle({
+    Name = "esp",
+    CurrentValue = false,
+    Flag = "Esp",
+    Callback = function(Value)
+        ESP.setVisible(Value)
+    end
+})
