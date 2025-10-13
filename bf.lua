bc550b02dea32b4a14d5477da34881d8dfb4495d
miureals <miu485926@gmail.com>
2025-10-13 08:54:08 +0700

    Update bf.lua

--- bf.lua
+++ bf.lua
@@ -306,3 +306,17 @@ local Toggle = testTab:CreateToggle({
             
    end,
 })
+
+local play = testTab:CreateSection("main")
+local lier = testTab:CreateSlider({
+   Name = "Slider Example",
+   Range = {0, 100},
+   Increment = 10,
+   Suffix = "Bananas",
+   CurrentValue = 10,
+   Flag = "dh", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(Value)
+   -- The function that takes place when the slider changes
+   -- The variable (Value) is a number which correlates to the value the slider is currently at
+   end,
+})
